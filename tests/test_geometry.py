5760c7b54ccdf27c4fe2363b83f39f8c7db1dedb
anglyan <angel.yanguas@gmail.com>
2018-12-30 15:13:18 -0600

    context for testing

--- tests/test_geometry.py
+++ tests/test_geometry.py
@@ -1,20 +1,22 @@
 import unittest
-import unitcell as uc
+
+from .context import geom3d as geom
+
 
 class TestVector(unittest.TestCase):
 
     def setUp(self):
-        self.v1 = uc.geometry.Vector(1, 2, 3)
-        self.v2 = uc.geometry.Vector(2, 2., 2)
-        self.v3 = uc.geometry.Vector(2, -1, 0)
+        self.v1 = geom.Vector(1, 2, 3)
+        self.v2 = geom.Vector(2, 2., 2)
+        self.v3 = geom.Vector(2, -1, 0)
 
     def test_sum(self):
         vs = self.v1 + self.v2
-        self.assertAlmostEqual(vs, uc.geometry.Vector(3, 4, 5))
+        self.assertAlmostEqual(vs, geom.Vector(3, 4, 5))
 
     def test_sub(self):
         vs = self.v1 - self.v2
-        self.assertAlmostEqual(vs, uc.geometry.Vector(-1, 0, 1))
+        self.assertAlmostEqual(vs, geom.Vector(-1, 0, 1))
 
     def test_copy(self):
         vs = self.v1.copy()
@@ -25,4 +27,5 @@ class TestVector(unittest.TestCase):
         self.assertIs(vs, self.v1)
 
 if __name__ == '__main__':
+
     unittest.main()
